861bf555b62430af695f6144ef649ee5a6f881ae
Zubenko1 <zubenkodp@gmail.com>
2022-08-20 14:58:30 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 var debounce = require('lodash.debounce');
-import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
